802ab7dab4d816db5b98035f18bfa3efd49c0dd4
sfw222 <2452042683@qq.com>
2026-07-27 15:04:52 +0800

    fix: update URL to own repo

--- zhihu-config.js
+++ zhihu-config.js
@@ -7,8 +7,8 @@
 // @match        https://www.zhihu.com/*
 // @match        https://zhuanlan.zhihu.com/*
 // @icon         https://cdn.jsdelivr.net/npm/remixicon@4.9.1/icons/Logos/zhihu-line.svg
-// @updateURL    https://raw.githubusercontent.com/SherlockChiang/Zhihu-auto-darkmode/main/zhihu-auto-dark-mode.user.js
-// @downloadURL  https://raw.githubusercontent.com/SherlockChiang/Zhihu-auto-darkmode/main/zhihu-auto-dark-mode.user.js
+// @updateURL    https://raw.githubusercontent.com/sfw222/zhihu-config/main/zhihu-config.js
+// @downloadURL  https://raw.githubusercontent.com/sfw222/zhihu-config/main/zhihu-config.js
 // @grant        GM_getValue
 // @grant        GM_setValue
 // @grant        GM_registerMenuCommand

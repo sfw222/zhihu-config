b32a402caca5c875eec151ac061856936b31ccfa
sfw222 <2452042683@qq.com>
2026-07-29 06:41:25 +0800

    三个元素深色适配 5.2

--- zhihu-config.user.js
+++ zhihu-config.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         知乎设置
 // @namespace    http://tampermonkey.net/
-// @version      5.1
+// @version      5.2
 // @description  手动切换知乎深色/浅色主题（默认深色），修复少量深色模式遗漏；完全移除 AI 总结卡片，不留空白占位。
 // @author       sfw222
 // @match        https://www.zhihu.com/*
@@ -97,6 +97,9 @@
             html[data-theme="dark"] .css-9511cm{background-color:#191b1f!important;border:1px solid rgba(255,255,255,.06)!important}
             html[data-theme="dark"] .css-dilx2p{background-color:#191b1f!important;border-color:var(--GBK09A,#282b30)!important}
             html[data-theme="dark"] .css-11oa45q{background-color:#191b1f!important;border:1px solid rgba(255,255,255,.06)!important}
+            html[data-theme="dark"] .css-2sopzd{background-color:#191b1f!important}
+            html[data-theme="dark"] .css-44kk6u{background-color:#191b1f!important}
+            html[data-theme="dark"] .css-1pariuy{background-color:#191b1f!important}
             html[data-theme="dark"] .Card{border:1px solid rgba(255,255,255,.06)!important}
             html[data-theme="dark"] .css-12tmx22{border:1px solid rgba(255,255,255,.06)!important}
             html[data-theme="dark"] .Comments-container .css-13445jb,
